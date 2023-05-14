7bbe8996ab2e548379a0ce2de0909e6bad18821b
st22112 <st22112@ormiston.school.nz>
2023-05-15 11:02:33 +1200

    update planning

---
+++ jph.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.geometry('700x700')
+root.resizable(False,False)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+root.mainloop()
